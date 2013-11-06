f6464455048c67cdbb4320f564c8bc9389b2c9ee
laff <olaf@highsoft.com>
2013-11-06 16:32:35 +0100

    sorting folder, creating array

--- index.php
+++ index.php
@@ -1,22 +1,43 @@
 <?php
 
-$jsonDir = "C:\Users\HS Vikar\Downloads\ball";
+// Directory of the json files containing our data,
+$jsonDir = "C:\Users\John\Dropbox\slettmeg\ball";
 
 
 $jsonDirStructure = scandir($jsonDir);
 
 $jsonFolders = array();
 
-foreach($jsonDirStructure as $jsonFolder) {
+$jsonArr = array();
 
-	echo $jsonFolder;
-	
-	if($jsonFolder != "." || $jsonFolder != "..") {
+// Find the foldernames that contains data by year.
+foreach ($jsonDirStructure as $jsonFolder) {
+
+	if ($jsonFolder != "." && $jsonFolder != "..") {
+
 		array_push($jsonFolders, $jsonFolder);
+		
+		$jsonArr[$jsonFolder] = array();
+
 	}
 }
 
-print_r($jsonFolders);
+
+// Finding the filenames within each folder.
+foreach ($jsonFolders as $jsonFolder) {
+
+	$folderDir = $jsonDir.'/'.$jsonFolder;
+
+	$jsonFolderStructure = scandir($folderDir);
+
+	foreach ($jsonFolderStructure as $jsonFile) {
+		if (filetype($folderDir.'/'.$jsonFile) == 'file') {
+			array_push($jsonArr[$jsonFolder], $jsonFile);
+		}
+	}
+}
+
+print_r($jsonArr);
 
 
 ?>
